9e71cb41f1156fff4d8bbb4e3a5528d50aeca6c1
pyro2927 <joseph@pintozzi.com>
2014-08-06 09:58:36 -0500

    Adding in Linux install script

--- install.sh
+++ install.sh
@@ -2,11 +2,23 @@
 #
 # Install nice things
 
-if test ! $(which brew)
-then
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+# check if OS X or linux
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+  if test ! $(which brew)
+  then
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  fi
+  brew install rbenv ruby-build fish tmux
+  chsh -s $(which fish) $(whoami)
+elif [[ "$unamestr" == 'Linux' ]]; then
+  sudo apt-get update
+  sudo apt-get install -y tmux fish git
+  if test ! $(which rbenv)
+  then
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+    echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  fi
 fi
-
-brew install rbenv ruby-build fish tmux
-
-chsh -s $(which fish) $(whoami)
