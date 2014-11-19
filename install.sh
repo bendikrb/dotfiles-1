a2a12059c43f47aeb7a8223a90b83e7b0c412c46
Joseph Pintozzi <joseph@pintozzi.com>
2014-11-18 19:28:21 -0600

    Updating homebrew install location

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
   if test ! $(which brew)
   then
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
   # install a few terminal basics
   brew install rbenv ruby-build fish tmux mercurial
